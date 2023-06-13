ed281d68f254813ebdda78b06b885ac25011ea69
aliabdela47 <ardii.2000@gmail.com>
2023-06-13 18:47:24 +0000

    db fixed

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
+    $ID = $_POST['ID'];
     $firstName = $_POST['firstName'];
     $lastName= $_POST['lastName'];
     $gender = $_POST['gender'];
@@ -6,13 +7,13 @@
     $password = $_POST['password'];
     $number = $_POST['number'];
    // Database connection
-   $conn = new mysqli('localhost','root','nica123@#','registration_form');
+   $conn = new mysqli('localhost','root','root','registration_form2');
    if($conn->connect_error){
     echo "$conn->connect_error";
     die("Connection Failed : ". $conn->connect_error);
    } else {
-    $stmt = $conn->prepare("INSERT INTO registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+    $stmt = $conn->prepare("INSERT INTO registration(ID, firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssssi",$ID, $firstName, $lastName, $gender, $email, $password, $number);
     $stmt->execute();
     echo "Registration successfully...";
     $stmt->close();
